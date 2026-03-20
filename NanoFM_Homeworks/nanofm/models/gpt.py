5965f235458b8ab54ccb936cb39750fd63f69ba5
DicksonYU-jpg <1155192668@link.cuhk.edu.hk>
2026-03-20 14:08:03 +0100

    fixed argument error from Layernorm

--- NanoFM_Homeworks/nanofm/models/gpt.py
+++ NanoFM_Homeworks/nanofm/models/gpt.py
@@ -67,7 +67,7 @@ class GPT(nn.Module):
                                       mlp_ratio=mlp_ratio,
                                       use_bias=use_bias) # TODO: Define the transformer trunk
         
-        self.out_norm = LayerNorm(dim, use_bias=use_bias) # TODO: Define the output layer normalization. Use the LayerNorm class defined in modeling/transformer_layers.py
+        self.out_norm = LayerNorm(dim, bias=use_bias) # TODO: Define the output layer normalization. Use the LayerNorm class defined in modeling/transformer_layers.py
         self.to_logits = nn.Linear(dim, vocab_size) # TODO: Define the output projection layer
 
         self.initialize_weights() # Weight initialization
